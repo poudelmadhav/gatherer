2f1afd7dc7c4f13415e1d19089f7d3d82a5e399d
Madhav Paudel <poudelmadhav143@gmail.com>
2019-12-25 15:52:11 +0545

    testing with selector using capybara

--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe "adding a project", type: :system do
     fill_in "Tasks", with: "Choose Fabric:3\nMake it work:5"
     click_on("Create Project")
     visit projects_path
-    expect(page).to have_content("Project Runway")
-    expect(page).to have_content(8)
+    @project = Project.find_by(name: "Project Runway")
+    expect(page).to have_selector("#project_#{@project.id} .name", text: "Project Runway")
+    expect(page).to have_selector("#project_#{@project.id} .total-size", text: "8")
   end
 end
