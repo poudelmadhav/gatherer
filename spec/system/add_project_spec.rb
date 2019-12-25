7b6a5cfbda6dab3cd37eecc561d6bdcd8dd8cae2
Madhav Paudel <poudelmadhav143@gmail.com>
2019-12-25 16:54:39 +0545

    rendering new page on empty submitting the name of project

--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe "adding a project", type: :system do
     expect(page).to have_selector("#project_#{@project.id} .name", text: "Project Runway")
     expect(page).to have_selector("#project_#{@project.id} .total-size", text: "8")
   end
+
+  it "does not allow a user to create a project without a name" do
+    visit new_project_path
+    fill_in "Name", with: ""
+    fill_in "Tasks", with: "Choose Fabric:3\nMake it work:5"
+    click_on("Create Project")
+    expect(page).to have_selector(".new_project")
+  end
 end
